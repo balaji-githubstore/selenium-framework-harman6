b4d85fad205c38fc2e1185a56dcdbd459f8182d5
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-04-20 11:41:05 +0530

    browser config

--- src/test/java/com/harman/test/LoginTest.java
+++ src/test/java/com/harman/test/LoginTest.java
@@ -1,33 +1,21 @@
 package com.harman.test;
 
-import java.time.Duration;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-public class LoginTest {
-
+import com.harman.base.WebDriverWrapper;
+	
+public class LoginTest extends WebDriverWrapper{
+	
 	@Test
 	public void validCredentialTest() {
-		
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-
-		driver.get("https://opensource-demo.orangehrmlive.com/");
 
 		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
 		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
 		driver.findElement(By.id("btnLogin")).click();
 
 		String actualUrl = driver.getCurrentUrl();
-
 		Assert.assertEquals(actualUrl, "https://opensource-demo.orangehrmlive.com/index.php/dashboard");
 	}
 
