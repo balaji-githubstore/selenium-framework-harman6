599854da65557de48d13d6fed0926f1237f41f28
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-04-20 12:39:31 +0530

    dataprovider for invalid test

--- src/test/java/com/harman/test/LoginTest.java
+++ src/test/java/com/harman/test/LoginTest.java
@@ -2,12 +2,13 @@ package com.harman.test;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.harman.base.WebDriverWrapper;
-	
-public class LoginTest extends WebDriverWrapper{
-	
+
+public class LoginTest extends WebDriverWrapper {
+
 	@Test
 	public void validCredentialTest() {
 
@@ -17,6 +18,30 @@ public class LoginTest extends WebDriverWrapper{
 
 		String actualUrl = driver.getCurrentUrl();
 		Assert.assertEquals(actualUrl, "https://opensource-demo.orangehrmlive.com/index.php/dashboard");
+	}
+
+	@DataProvider
+	public String[][] invalidCredentialData() {
+		String[][] main = new String[2][3];
+		main[0][0] = "john";
+		main[0][1] = "john123";
+		main[0][2] = "Invalid credentials";
+
+		main[1][0] = "peter";
+		main[1][1] = "peter123";
+		main[1][2] = "Invalid credentials";
+
+		return main;
+	}
+
+	@Test(dataProvider = "invalidCredentialData")
+	public void invalidCredentialTest(String username, String password, String expectedError) {
+		driver.findElement(By.id("txtUsername")).sendKeys(username);
+		driver.findElement(By.id("txtPassword")).sendKeys(password);
+		driver.findElement(By.id("btnLogin")).click();
+
+		String actualError = driver.findElement(By.id("spanMessage")).getText();
+		Assert.assertEquals(actualError, expectedError);
 	}
 
 }
