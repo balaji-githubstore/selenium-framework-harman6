85948df2ffe2e4e1c720ccab328a19717abab032
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-04-20 11:23:10 +0530

    title and valid credential test

---
+++ src/test/java/com/harman/test/LoginUserInterfaceTest.java
@@ -0,0 +1,34 @@
+package com.harman.test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginUserInterfaceTest {
+	
+	
+	
+	@Test
+	public void validateTitleTest()
+	{
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		String actualTitle=driver.getTitle();
+		
+		Assert.assertEquals(actualTitle, "OrangeHRM");
+	}
+
+
+}
